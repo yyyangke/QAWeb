621df2e1ae3b38a524dc7584f1b22a550f431733
yyyangke <yyyang011@gmail.com>
2016-08-16 16:52:33 +0800

    修改

--- src/main/java/com/yangke/controller/IndexController.java
+++ src/main/java/com/yangke/controller/IndexController.java
@@ -1,19 +1,29 @@
 package com.yangke.controller;
 
 
+import com.yangke.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by yangke on 16/8/15.
  */
 @Controller
 public class IndexController {
-    @RequestMapping(path ={"/","/index/{userId}"},method = {RequestMethod.GET})
-    @ResponseBody
-    public String index(@PathVariable("userId") int userId,
-                        @RequestParam(value="type",defaultValue="1",required = false)  int type) {
-        return String.format("is %d %d",userId,type);
+
+
+    @RequestMapping(path ={"/","/index"},method = {RequestMethod.GET})
+    public String index(Model model) {
+        model.addAttribute("h1","j2");
+        List<String> l = Arrays.asList(new String[] {"hong","huang"});
+        model.addAttribute("colors",l);
+        User u = new User("huhu");
+        model.addAttribute("h3",u);
+        return "header";
     }
 
 }
